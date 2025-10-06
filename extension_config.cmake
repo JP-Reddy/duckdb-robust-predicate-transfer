9a030d77f81c703b4d39e90251d4600b1b334cd2
JP-Reddy <guthijp.reddy@gmail.com>
2025-10-06 00:27:38 -0500

    convert to dynamic extension instead of static

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,9 +1,10 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
 
-# Extension from this repo
+# Extension from this repo - build as dynamic extension 
 duckdb_extension_load(rpt
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
+    DONT_LINK
 )
 
 # Any extra extensions that should be built
